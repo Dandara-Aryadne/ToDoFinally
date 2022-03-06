aad827f2fa084789f4ee11f7733c9bb6d7e55105
Dandara Aryadne <94024628+Dandara-Aryadne@users.noreply.github.com>
2022-03-05 22:04:39 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import "./styles.css";
+export default class ToDo extends Component {
+  state = {
+    task: "",
+    taskList: []
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      task: event.target.value
+    });
+  };
+
+  add = () => {
+    let { task, taskList } = this.state;
+    if (task !== "") {
+      this.setState({
+        taskList: taskList.concat({
+          task,
+          id: Date.now()
+        }),
+        task: ""
+      });
+    }
+  };
+
+  remove = (id) => {
+    let { taskList } = this.state;
+    this.setState({
+      taskList: taskList.filter((item) => item.id !== id)
+    });
+  };
+
+  render() {
+    let { handleChange, add, remove } = this;
+    let { task, taskList } = this.state;
+    return (
+      <div>
+        <h1>SCHEDULE</h1>
+        <p>Get Organized</p>
+        <div class="box">
+          <input
+            placeholder="TYPE"
+            value={task}
+            onChange={handleChange}
+            type="text"
+          />
+        </div>
+        <div class="btn1">
+          <button onClick={add}>ADD</button>
+        </div>
+        <div class="first">
+          {taskList.map((item) => (
+            <ul>
+              <li>{item.task}</li>
+              <img
+                class="trash"
+                onClick={() => remove(item.id)}
+                alt="trashpoint"
+                src="https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes/128/trash-circle-red-512.png"
+              />
+            </ul>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
